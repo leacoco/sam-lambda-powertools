ba30a61d36a47df7f40149befe29630dee9ca688
Leadel Ngalame <leadel.ngalame@otto.de>
2022-06-25 05:41:52 +0200

    Add tracing

--- powertools-quickstart/hello_world/app.py
+++ powertools-quickstart/hello_world/app.py
@@ -3,6 +3,7 @@ import json
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
+from aws_xray_sdk.core import xray_recorder
 
 logger = Logger(service="Hello-App")
 app = APIGatewayRestResolver()
@@ -11,12 +12,14 @@ app = APIGatewayRestResolver()
 
 
 @app.get("/hello/<name>")
+@xray_recorder.capture("hello_name")
 def hello_name(name):
     logger.info(f"Request for {name} received")
     return {"message": f"Welcome {name}"}
 
 
 @app.get("/hello")
+@xray_recorder.capture("hello")
 def hello():
     logger.info("Request from unknown")
     return {"message": "Hello unknown"}
@@ -45,6 +48,7 @@ def hello():
 @logger.inject_lambda_context(
     correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True
 )
+@xray_recorder.capture("handler")
 def lambda_handler(event, context):
     """Sample pure Lambda function"""
     return app.resolve(event, context)
