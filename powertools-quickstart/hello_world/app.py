feea8f61aa8d18c0249967e7fd1ccfe93dde1f94
Leadel Ngalame <leadel.ngalame@otto.de>
2022-06-25 04:25:28 +0200

    use powertools and apigateway resolver

--- powertools-quickstart/hello_world/app.py
+++ powertools-quickstart/hello_world/app.py
@@ -10,48 +10,41 @@ app = APIGatewayRestResolver()
 # import requests
 
 
-def hello_name(event, **kwargs):
-    username = event["pathParameters"]["name"]
-    return {"statusCode": 200, "body": json.dumps({"message": f"Welcome {username}"})}
+@app.get("/hello/<name>")
+def hello_name(name):
+    logger.info(f"Request for {name} received")
+    return {"message": f"Welcome {name}"}
 
 
-def hello(**kwargs):
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": f"hello Unknown",
-            }
-        ),
-    }
+@app.get("/hello")
+def hello():
+    logger.info("Request from unknown")
+    return {"message": "Hello unknown"}
 
 
-@dataclass
-class Router:
-    routes: dict = field(default_factory=dict)
+# @dataclass
+# class Router:
+#     routes: dict = field(default_factory=dict)
 
-    def set(self, path, method, handler):
-        self.routes[f"{path}-{method}"] = handler
+#     def set(self, path, method, handler):
+#         self.routes[f"{path}-{method}"] = handler
 
-    def get(self, path, method):
-        try:
-            route = self.routes[f"{path}-{method}"]
-        except KeyError:
-            raise RuntimeError("method or path not found")
-        return route
-
-
-router = Router()
-router.set(path="/hello", method="GET", handler=hello)
-router.set(path="/hello/{name}", method="GET", handler=hello_name)
+#     def get(self, path, method):
+#         try:
+#             route = self.routes[f"{path}-{method}"]
+#         except KeyError:
+#             raise RuntimeError("method or path not found")
+#         return route
 
 
+# router = Router()
+# router.set(path="/hello", method="GET", handler=hello)
+# router.set(path="/hello/{name}", method="GET", handler=hello_name)
+
+
+@logger.inject_lambda_context(
+    correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True
+)
 def lambda_handler(event, context):
     """Sample pure Lambda function"""
-    path = event["resource"]
-    http_method = event["httpMethod"]
-
-    handler = router.get(path, http_method)
-
-    print(handler)
-    return handler(event=event)
+    return app.resolve(event, context)
